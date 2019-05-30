58a39382c6236d8c881ff173533c2c790e1faac7
zenoven <zenoven@gmail.com>
2019-05-30 09:38:49 +0800

    chore: rename method and format code with trailling comma

--- lib/main.dart
+++ lib/main.dart
@@ -23,31 +23,36 @@ class RandomWordsState extends State<RandomWords> {
 
   final _larggerFont = const TextStyle(fontSize: 18.0);
 
-  Widget _buildWordList() {
+  Widget _buildSuggestions() {
     return new ListView.builder(
-        padding: const EdgeInsets.all(20.0),
-        itemBuilder: (ctx, i) {
-          if (i.isOdd) return new Divider();
-          final index = i ~/ 2;
-          if (index >= _wordList.length) {
-            _wordList.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_wordList[index], index);
-        });
+      padding: const EdgeInsets.all(20.0),
+      itemBuilder: (ctx, i) {
+        if (i.isOdd) return new Divider();
+        final index = i ~/ 2;
+        if (index >= _wordList.length) {
+          _wordList.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_wordList[index], index);
+      },
+    );
   }
 
   Widget _buildRow(WordPair pair, int index) {
     return new ListTile(
-        title:
-            new Text('${index + 1}. ${pair.asPascalCase}', style: _larggerFont));
+      title: new Text(
+        '${index + 1}. ${pair.asPascalCase}',
+        style: _larggerFont,
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext ctx) {
     return new Scaffold(
-        appBar: new AppBar(
-          title: new Text('hello inner title'),
-        ),
-        body: _buildWordList());
+      appBar: new AppBar(
+        title: new Text('hello inner title'),
+      ),
+      body: _buildSuggestions(),
+    );
   }
 }
