b060c26656454f76da965a3a01523daa50b7f14d
zenoven <zenoven@gmail.com>
2019-06-18 19:38:31 +0800

    fix: rename file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
-import 'package:flutter_demo/detai.dart';
 import 'package:flutter_demo/detail.dart';
 import 'package:flutter_demo/saved.dart';
 
