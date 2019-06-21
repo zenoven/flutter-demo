2909737709cbec0aa6155f51816c7a1ea3e9c1bb
zenoven <zenoven@gmail.com>
2019-06-21 18:01:21 +0800

    chore: reorganize files

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
-import 'package:flutter_demo/detail.dart';
-import 'package:flutter_demo/saved.dart';
-import 'package:flutter_demo/test.dart';
+import 'package:flutter_demo/screens/detail.dart';
+import 'package:flutter_demo/screens/saved.dart';
+import 'package:flutter_demo/screens/test.dart';
 
 final _iconMap = {
   'saved': new Icon(
